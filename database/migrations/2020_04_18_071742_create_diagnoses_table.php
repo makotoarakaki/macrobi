dc2394943568ae48e676256d572365ade9851a02
makotoarakaki <marakaki0419@gmail.com>
2020-04-21 20:30:36 +0900

    table-diagnoses
    Signed-off-by: makotoarakaki <marakaki0419@gmail.com>

--- database/migrations/2020_04_18_071742_create_diagnoses_table.php
+++ database/migrations/2020_04_18_071742_create_diagnoses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDiagnosesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('diagnoses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->string('feature');
+            $table->integer('yousei');
+            $table->integer('chuyou');
+            $table->integer('insei');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('diagnoses');
     }
 }
